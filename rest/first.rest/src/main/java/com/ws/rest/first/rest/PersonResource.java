df8e6a84820c3c1cc24c506ae1e66cf1daced0be
Umakanth Pai <upai@in-technology.de>
2020-04-25 02:23:53 +0200

    JSON, removed old target dir

--- rest/first.rest/src/main/java/com/ws/rest/first/rest/PersonResource.java
+++ rest/first.rest/src/main/java/com/ws/rest/first/rest/PersonResource.java
@@ -1,10 +1,12 @@
 package com.ws.rest.first.rest;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -40,6 +42,19 @@ public class PersonResource {
 		
 		return repo.getPersons().get(0);
 	}
+	
+	@GET
+	@Path("getperson/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Person getPerson(@PathParam("id") int id) {
+		
+		System.out.println("called getFirstPerson...");
+		 Optional<Person> opt = repo.getPerson(id); 
+		 if(opt.isPresent()) {
+			 return opt.get();
+		 }
+		return null;
+	}	
 	
 	@POST
 	@Path("createperson")
